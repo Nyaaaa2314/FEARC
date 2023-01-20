8d0b322abc86a5ec7dfd8928208d9228827a5fa7
Jay <Jadiel.r25@gmail.com>
2023-01-20 01:33:59 -0500

    Done for the night

--- FEARC/Class.java
+++ FEARC/Class.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Class {
 	public String name;
@@ -6,12 +7,15 @@ public class Class {
 	public String wb;
 	public String wc;
 	public String JID;
-	public Class(String name, boolean promoted, String wa, String wb, String wc, String JID) {
+	public boolean flier;
+	public ArrayList<String> promotions;
+	public Class(String name, boolean promoted, String wa, String wb, String wc, String JID, boolean flier) {
 		this.name = name;
 		this.promoted = promoted;
 		this.wa = wa;
 		this.wb = wb;
 		this.wc = wc;
 		this.JID = JID;
+		this.flier = flier;
 	}
 }
