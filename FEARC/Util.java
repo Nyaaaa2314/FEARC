0801659489a379f3d920f5be24186b2e498ace52
Jay Rios <jadiel.r25@gmail.com>
2023-02-10 22:50:52 -0500

    fixed random lol

--- FEARC/Util.java
+++ FEARC/Util.java
@@ -42,143 +42,53 @@ public class Util<T> {
 		
 		return ret;
 	}
-	public static void shufflePromotes(ArrayList<String> all){
-		//ArrayList<Class> classes = Data.classes;
-		Stack<String> A = new Stack<String>();
-		ArrayList<String> B = new ArrayList<String>();
-		Stack<String> C = new Stack<String>();
-		for(Class c : Data.classes) {
-			c.pa = 0;
-			for(String s : all) {
-				if(c.promotions != null) {
-					if(!c.promotions.contains(s)) {
-						c.pa++;
-					}
-				}
-			}
-			
-		}
-		ArrayList<Class> cl = Data.classes;
-		ArrayList<Class> copy = new ArrayList<Class>();
-		for(Class c : cl) {
-			copy.add(c);
-		}
-//		classes.sort((Class a, Class b) -> a.pa - b.pa);
-//		for(Class c : classes) {
-//			System.out.println(c.pa);
-//		}
-		Collections.shuffle(all);
-		for(String s : all) {
-			C.push(s);
-		}
-		int in = 0;
-		for(Class c : copy) {
-			if(c.promotions ==  null) {
-				continue;
-			}
-			ArrayList<String> pr = new ArrayList<String>();
-			//cl.get(in).promotions = new ArrayList<String>();
-			int add = 0;
-			while(add < 1) {
-				if(!c.promotions.contains(C.peek())) {
-					pr.add(C.pop());
-					add++;
-				}
-				else {
-					A.push(C.pop());
-				}
-			}
-			Class temp = cl.get(in);
-			temp.promotions = pr;
-			cl.set(in, temp);
-			in++;
-		}
-		while(!C.isEmpty()) {
-			A.push(C.pop());
-		}
-		while(!A.isEmpty()) {
-			String s = A.pop();
-			if(A.contains(s)) {
-				B.add(s);
-			}
-			else {
-				C.push(s);
-			}
-		}
-		while(!C.isEmpty()) {
-			A.push(C.pop());
-		}
-		in = 0;
-		for(Class c : copy) {
-			if(c.promotions == null) {
-				in++;
-				continue;
-			}
-			String s = cl.get(in).promotions.get(0);
-			Boolean add = false;
-			while(!add) {
-				if(A.isEmpty()) {
-					A.addAll(B);
-				}
-				if(A.peek().equals(s) || c.promotions.contains(A.peek())) {
-					C.push(A.pop());
-				}
-				else {
-					cl.get(in).promotions.add(A.pop());
-					in++;
-					while(!C.isEmpty()) {
-						A.push(C.pop());
-					}
-					add = true;
-					if(B.contains(s)) {
-						B.remove(s);
-						A.push(s);
-					}
-				}
-			}	
-		}
-		System.out.println(validSort(copy, cl));
-		System.exit(0);
+	
+	public static void shufflePromotes(ArrayList<String> all) {
 		
-		//return all;
+		Stack<String> A = new Stack<String>();
+		//double start = System.currentTimeMillis();
+		ArrayList<ArrayList<String>> shuffle = new ArrayList<ArrayList<String>>();
+		//int runs = 0;
+		while(!validShuffle(shuffle)) {
+			Collections.shuffle(all);
+			shuffle = new ArrayList<ArrayList<String>>();
+			for(int i = 0; i < all.size(); i++) {
+				ArrayList<String> temp = new ArrayList<String>(2);
+				temp.add(all.get(i++));
+				temp.add(all.get(i));
+				shuffle.add(temp);
+			}
+			//runs++;
+		}
+		//double end = System.currentTimeMillis();
+		//System.out.println("Took " + (end - start) / 1000 + " seconds and took " + runs + " permutations");
+		//System.exit(0);
 	}
-	public static boolean validSort(ArrayList<Class> a, ArrayList<Class> b) throws IllegalStateException{
-		int dupes = 0;
-		for(int i = 0; i < a.size(); i++) {
-			if(a.get(i) == null && b.get(i) == null) {
+	
+	public static boolean validShuffle(ArrayList<ArrayList<String>> shuffle) {
+		if(shuffle.size() == 0) {
+			return false;
+		}
+		int i = 0;
+		for(Class c : Data.classes) {
+			if(c.promotions == null) {
 				continue;
 			}
-			else if(a.get(i) != null && b.get(i) != null) {
-				ArrayList<String> as = a.get(i).promotions;
-				ArrayList<String> bs = b.get(i).promotions;
-				if(bs.size() != 2) {
-					System.out.println("Wrong # of promotions");
-					return false;
-				}
-				if(bs.get(0).equals(bs.get(1))) {
-					//dupes++;
-					//return false;
-					System.out.println("Duplicate entry in resulting pair ");
-					return false;
-				}
-				if(as.contains(bs.get(0)) || as.contains(bs.get(1))) {
-					dupes++;
-					//System.out.println("Not distinct from original pair");
-					//return false;
-				}
+			ArrayList<String> ch = c.promotions;
+			ArrayList<String> sh = shuffle.get(i++);
+			if(sh.get(0).equals(sh.get(1))) {
+				return false;
 			}
-			else {
-				System.out.println("Data at wrong indices");
+			if(ch.contains(sh.get(0)) || ch.contains(sh.get(1))) {
 				return false;
 			}
 		}
-		if(dupes > 0) {
-			System.out.println("Not distinct from original pair " + dupes);
-			return false;
-		}
-		
 		return true;
 	}
+	
+	
+	
+	
 	
 	public static <T>ArrayList<T> ensureShuffle(ArrayList<T> all){
 		boolean shuffled = false;
